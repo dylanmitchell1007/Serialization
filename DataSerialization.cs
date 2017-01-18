9f6e66a352429f5debe3fe3d12a0bfc4de41bc68
dylanmitchell1007 <armadummy@yahoo.com>
2017-01-18 17:48:34 -0600

    Changes

---
+++ DataSerialization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Serialize
+{
+    class DataSerialization
+    {
+        private static object serilaizer;
+
+        public static void Serialize(string fileName T data)
+        {
+            XmlSerializer serilize = new XmlSerializer(typeof(T));
+            TextWriter writter = new StreamWriter(@"..\..\SavedFiles\"+ fileName + ".xml");
+            serilaizer.Serialize(writter, data);
+
+            writter.Close();
+
+
+        }
+
+
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    class Student
+    {
+       
+       private string mName;
+       private int mAge;
+       private string mID;
+
+        public Student(){ }
+
+        public Student(string student, int a, string)
+        {
+
+        }
+
+        public string Name;
+        public string AGE;
+        public string ID;
+    }
+}
